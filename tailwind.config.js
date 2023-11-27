90a083da6c275d0744026c9df436ecff03a869ca
ibnufauzan <ibnufauzan890@gmail.com>
2023-11-27 14:17:17 +0700

    a

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['index.html'],
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "food-blog/aset/lily-banse--YHSwy6uqvk-unsplash 1.png"
+        }
+      },
+    },
+    plugins: [],
+  }
